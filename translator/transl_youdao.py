1e176d91729e1982ea42e3e2ae596d728a131f88
mtrdong <mtrdong@qq.com>
2022-09-12 20:16:01 +0800

    有道词典爬虫新增获取发音和例句

--- translator/transl_youdao.py
+++ translator/transl_youdao.py
@@ -22,6 +22,7 @@ class YoudaoTranslate(object):
                           'AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/105.0.0.0 Safari/537.36',
         }
+        self.data = None
 
     @staticmethod
     def get_form_data(text, le):
@@ -46,19 +47,36 @@ class YoudaoTranslate(object):
         }
         return form_data
 
+    def get_sentence(self):
+        """获取例句"""
+        sentence_list = []
+        try:
+            sentence_pair = self.data["blng_sents_part"]["sentence-pair"]
+            for item in sentence_pair:
+                sentence_list.append({
+                    'sentence': item.get('sentence-eng') or item.get('sentence'),
+                    'translation': item.get('sentence-translation'),
+                    'speech': 'audio=' + (item.get('sentence-speech') or item.get('sentence-translation-speech')),
+                })
+        except KeyError:
+            return sentence_list
+        return sentence_list
+
+    def get_voice(self, params):
+        """获取发音"""
+        url = 'https://dict.youdao.com/dictvoice'
+        response = requests.get(url, params=params, headers=self.headers)
+        content = response.content
+        return content
+
     def translate(self, query, le):
         """翻译"""
         form_data = self.get_form_data(query, le)
         response = requests.post(self.url, data=form_data, headers=self.headers)
-        data = response.json()
-        print()
-        print(f'音标：英/{data["ec"]["word"]["ukphone"]}/ 美/{data["ec"]["word"]["usphone"]}/')
-        print(f'释义：{data["ec"]["word"]["trs"]}')
-        print(f'词形：{data["expand_ec"]["word"][0]["wfs"] + data["expand_ec"]["word"][1]["wfs"]}')
-        print(f'例句：{data["blng_sents_part"]["sentence-pair"]}')
-        return data
+        self.data = response.json()
 
 
 if __name__ == '__main__':
     yt = YoudaoTranslate()
-    yt.translate('good', 'en')
+    yt.translate('你好', 'en')
+    sentence = yt.get_sentence()
