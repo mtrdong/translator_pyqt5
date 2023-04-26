820c9587712282e06008abae053e1ebf872645a9
mtrdong <mtrdong@qq.com>
2023-04-26 23:50:47 +0800

    修复百度翻译单词发音

--- translator/spider/__init__.py
+++ translator/spider/__init__.py
@@ -7,7 +7,7 @@ class BaseTranslate(object):
 
     def __init__(self):
         # 创建客户端并设置客户端UA
-        self.session = httpx.Client()
+        self.session = httpx.Client(verify=False)
         self.session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' \
                                              'AppleWebKit/537.36 (KHTML, like Gecko) ' \
                                              'Chrome/111.0.0.0 Safari/537.36'
